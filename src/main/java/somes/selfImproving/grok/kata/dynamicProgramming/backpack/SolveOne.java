788d314d82d60f2b4d4f601f3af584bc99de6e8f
Andrii Metelskyi <metelskyi@gmail.com>
2020-11-20 16:10:08 +0300

    task

--- src/main/java/somes/selfImproving/grok/kata/dynamicProgramming/backpack/SolveOne.java
+++ src/main/java/somes/selfImproving/grok/kata/dynamicProgramming/backpack/SolveOne.java
@@ -1,8 +1,9 @@
 package somes.selfImproving.grok.kata.dynamicProgramming.backpack;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Set;
 
 public class SolveOne implements IBackpackChoose {
     @Override
@@ -16,7 +17,7 @@ public class SolveOne implements IBackpackChoose {
             then come second field, here we can have 3 cases:
                 1. second item can't be placed -- just put thing from previous row in same column
                   second item can be placed, calculate value plus get value of free cells from previous row and compare with prev col val:
-                2. it is better to place second item -- put it with something from previous row
+                2. it is better to place second item -- put it with something(calculate it) from previous row
                 3. it is better to get from previous row -- just get it
              repeat this for all other things
              at the last row and last column we will have best result, why? think for a minute with urchin
@@ -28,40 +29,56 @@ public class SolveOne implements IBackpackChoose {
         //if can't be placed, fill with prev row if it is not first row
         //else calculate this item with rest from prev and compare with just prev row, choose best
 
-        List<List<List<BackpackItem>>> table = new ArrayList<>(capacity);
+        List<List<Set<BackpackItem>>> table = new ArrayList<>(capacity);
         for (int i = 0; i < items.size(); i++) {
             BackpackItem item = items.get(i);
 
-            List<List<BackpackItem>> row = new ArrayList<>();
+            List<Set<BackpackItem>> row = new ArrayList<>();
             for (int j = 0; j < capacity; j++) {
-                int curCapacity = j+1;
-                if(item.getSize() >  curCapacity) {
-                    List<BackpackItem> cell = (i == 0) ? new ArrayList<>() : table.get(i-1).get(j);
+                int curCapacityHuman = j+1;
+                if(item.getSize() >  curCapacityHuman) {
+                    Set<BackpackItem> cell = (i == 0) ? new HashSet<>() : table.get(i-1).get(j);
                     row.add(cell);
                 } else {
-//                    int currentPrice = item.getCost();
-                    int leftCapacity = curCapacity - item.getSize();
+                    if(i ==0) {
+                        row.add(Set.of(item));
+                        continue;
+                    }
 
-                    int prevFullPrice = table.get(i-1).get(curCapacity -1).stream().map(BackpackItem::getCost).mapToInt(Integer::intValue).sum();
+                    int leftCapacity = curCapacityHuman - item.getSize();
+
+                    int prevFullPrice = table.get(i-1).get(curCapacityHuman -1).stream().map(BackpackItem::getCost).mapToInt(Integer::intValue).sum();
                     int prevLeftoverPrice = (leftCapacity == 0) ? 0 : table.get(i - 1).get(leftCapacity - 1).stream().map(BackpackItem::getCost).mapToInt(Integer::intValue).sum();
 
-                    int currentPrice = item.getCost() + prevLeftoverPrice;
+                    int currentMaxPrice = item.getCost() + prevLeftoverPrice;
 
-
+                    if(prevFullPrice > currentMaxPrice) {
+                        //place everything from previous row
+                        row.add(table.get(i-1).get(curCapacityHuman-1));
+                    } else {
+                        Set<BackpackItem> cell = new HashSet<>(Set.of(item));
+                        if(leftCapacity > 0) cell.addAll(table.get(i - 1).get(leftCapacity-1));
+                        row.add(cell);
+                    }
                 }
             }
             table.add(row);
-
-
         }
 
+        //get last cell and see, what inside
+        Set<BackpackItem> lastCell = table.get(items.size() -1).get(capacity -1);
+        int sum  = lastCell.stream().mapToInt(BackpackItem::getCost).sum();
+        int totalPlaces  = lastCell.stream().mapToInt(BackpackItem::getSize).sum();
+        lastCell.forEach(b -> System.out.println(b.toString()));
+
+        System.out.println("sum = " + sum);
+        System.out.println("totalPlaces = " + totalPlaces);
 
 
 
 
 
 
-
-        return -1;
+        return sum;
     }
 }
--- src/main/java/somes/selfImproving/grok/kata/dynamicProgramming/backpack/Test.java
+++ src/main/java/somes/selfImproving/grok/kata/dynamicProgramming/backpack/Test.java
@@ -6,24 +6,40 @@ import static java.util.Arrays.asList;
 
 public class Test {
     public static void main(String[] args) {
-        var water = BackpackItem.builder().name("water").size(3).cost(10).build();
-        var book  = BackpackItem.builder().name("book").size(1).cost(3).build();
-        var food  = BackpackItem.builder().name("food").size(2).cost(9).build();
-        var dress = BackpackItem.builder().name("dress").size(2).cost(5).build();
-        var camera = BackpackItem.builder().name("camera").size(1).cost(6).build();
+        IBackpackChoose solveOne = new SolveOne();
 
-        final int RESULT = -1;
+        List<BackpackItem> items = asList(
+                BackpackItem.builder().name("water").size(3).cost(10).build(),
+                BackpackItem.builder().name("book").size(1).cost(3).build(),
+                BackpackItem.builder().name("food").size(2).cost(9).build(),
+                BackpackItem.builder().name("dress").size(2).cost(5).build(),
+                BackpackItem.builder().name("camera").size(1).cost(6).build());
 
-        List<BackpackItem> items = asList(water, book, food, dress, camera);
+        int capacity = 6;
+        int result = 25;
 
-        SolveOne solveOne = new SolveOne();
-        if(solveOne.maxCost(items, 7) != RESULT) {
-            throw new RuntimeException("Something is going wrong, let's check what!");
-        }
+        test(solveOne, items, capacity, result);
+
+        items = asList(
+                BackpackItem.builder().name("guitar").size(1).cost(1500).build(),
+                BackpackItem.builder().name("boombox").size(4).cost(3000).build(),
+                BackpackItem.builder().name("laptop").size(3).cost(2000).build(),
+                BackpackItem.builder().name("iphone").size(1).cost(2000).build(),
+                BackpackItem.builder().name("mp3").size(1).cost(1000).build()
+        );
+        capacity = 4;
+        result = 4500;
+
+        test(solveOne, items, capacity, result);
 
         System.out.println("SUCCESS!!!");
     }
 
+    private static void test(IBackpackChoose solve, List<BackpackItem> items, int capacity, int result) {
+        if (solve.maxCost(items, capacity) != result) {
+            throw new RuntimeException("Something is going wrong, let's check what!");
+        }
+    }
 
 /* todo
     -- NEXT STEP IS RUN JAVA 11, RUN THIS TEST
